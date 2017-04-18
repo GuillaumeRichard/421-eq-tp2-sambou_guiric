3ecd5f06f29d70d36d22364a73aeb2ba7e9712b3
samuelBoucher <samuel.boucher@outlook.com>
2017-04-18 10:35:45 -0400

    Google Api fonctionne

--- recherche/src/app/app.module.ts
+++ recherche/src/app/app.module.ts
@@ -1,20 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { FormsModule } from "@angular/forms";
+import { BrowserModule  } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NguiMapModule} from '@ngui/map';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyAOgYOBdHV3dc4M0bevQIpJ7MvgSyJ9GIU'})
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
