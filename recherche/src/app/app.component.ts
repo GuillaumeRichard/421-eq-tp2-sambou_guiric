12386b046f665a9a6934f62d784be96bf8fbaaea
samuelBoucher <samuel.boucher@outlook.com>
2017-04-18 21:14:37 -0400

    Voici comment ajouter un marqueur

--- recherche/src/app/app.component.ts
+++ recherche/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Google Maps API';
+  positions = [];
+
+  onClick(event) {
+      if (event instanceof MouseEvent)
+        return;
+      this.positions.push(event.latLng);
+      event.target.panTo(event.latLng);
+  }
+
 }
