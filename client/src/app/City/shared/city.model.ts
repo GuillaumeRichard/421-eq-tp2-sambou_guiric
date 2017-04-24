753c457bc72bade61947bd5492541cd5f3b6a131
samuelBoucher <samuel.boucher@outlook.com>
2017-04-24 19:57:43 -0400

    Merge de la branche client

---
+++ client/src/app/City/shared/city.model.ts
@@ -0,0 +1,9 @@
+import Poi from '../../POI/shared/poi.model';
+
+export class City {
+  Id: number;
+  Name: string;
+  Country: string;
+  Population: number;
+  PoiList: Poi[];
+}
---
+++ client/src/app/POI/shared/poi.model.ts
@@ -0,0 +1,9 @@
+export default class Poi {
+  Id: number;
+  Name: string;
+  Description: string;
+  Address: string;
+  Longitude: string;
+  Latitude: string;
+  CityId: number;
+}
