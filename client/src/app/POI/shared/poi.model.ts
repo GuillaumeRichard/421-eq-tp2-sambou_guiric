ae5897c36835a79918856419c71db3d9fe4131ce
samuelBoucher <samuel.boucher@outlook.com>
2017-04-25 11:16:04 -0400

    Suppression classe poi-default

--- client/src/app/POI/shared/poi.model.ts
+++ client/src/app/POI/shared/poi.model.ts
@@ -7,18 +7,3 @@ export class Poi {
   Latitude: string;
   CityId: number;
 }
-
-export default class PoiDefault {
-  Id: number;
-  Name: string;
-  Description: string;
-  Address: string;
-  Longitude: string;
-  Latitude: string;
-  CityId: number;
-}
-
-// Default: pour le city.model
-// sans Default: pour poi-list.component
-// je ne sais pas pourquoi ça marche pas vice-versa
-
