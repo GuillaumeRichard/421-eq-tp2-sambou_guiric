29fd7f49d214ab334d27c7447914cebcca418872
GuillaumeRichard <guillaume.richard00@gmail.com>
2017-04-25 10:53:17 -0400

    install @ngui/map

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NguiMapModule } from '@ngui/map';
 
 import { AppComponent } from './app.component';
 import { CitySearchComponent } from './City/city-search/city-search.component';
 import { MapComponent } from './map/map.component';
-import { NguiMapModule } from '@ngui/map';
 import { PoiListComponent } from './POI/poi-list/poi-list.component';
 import { PoiDetailComponent } from './POI/poi-detail/poi-detail.component';
 
