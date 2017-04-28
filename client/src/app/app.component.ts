95b78b0df38908b2e0b30575cc2bf30e9bc4e78d
samuelBoucher <samuel.boucher@outlook.com>
2017-04-28 08:06:14 -0400

    Bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'City POI';
+  title = 'Bienvenue à AirBnb version cheap!';
 }
