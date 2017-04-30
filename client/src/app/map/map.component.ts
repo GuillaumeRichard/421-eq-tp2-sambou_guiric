d938e21d0b0a9ce09d8fad35e83315c12045d9ac
samuelBoucher <samuel.boucher@outlook.com>
2017-04-30 11:53:49 -0400

    Afficher liste poi sur carte

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -7,47 +7,30 @@ import {Poi} from "../POI/shared/poi.model";
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit{
+  DEFAULT_CITY = "Quebec";
+
   positions = [];
   selectedPos: Position;
   poiService: PoiService;
-  poiList: Poi[] = [
-  {id: 1,
-    name: 'Cegep Ste-Foy',
-    description: 'Le cegep le plus cool',
-    address: '2700, ch. Ste-Foy',
-    longitude: '46.78589761',
-    latitude: '-71.28661394',
-    cityId: 1},
-
-  {id: 2,
-    name: 'Château Frontenac',
-    description: 'L\'icône de la ville de Québec',
-    address: '2700, ch. Ste-Foy',
-    longitude: '46.810756',
-    latitude: '-71.2044479',
-    cityId: 1},
-
-  {id: 3,
-    name: 'Île Madame',
-    description: 'Le lieu où se déroule le meilleur roman \'Les grandes marées\'',
-    address: '2700, ch. Ste-Foy',
-    longitude: '46.971245',
-    latitude: '-70.7868957',
-    cityId: 1},
-];
+  poiList: Poi[];
 
   constructor(poiService: PoiService){
     this.poiService = poiService;
   }
 
   ngOnInit(): void {
-    this.getPoiPositions()
+    this.getPoiList(this.DEFAULT_CITY).then(() => this.getPoiPositions());
+    // this.getPoiPositions();
+  }
+
+  getPoiList(name: string): Promise<Poi[]> {
+    return this.poiService.getPoiList(name).then(poiList => this.poiList = poiList);
   }
 
   getPoiPositions() {
-    for (var i = 0, length = this.poiList.length; i < length; i++)
+    for (let i = 0, length = this.poiList.length; i < length; i++)
     {
-      var poi = this.poiList[i];
+      let poi = this.poiList[i];
       this.positions.push([poi.longitude, poi.latitude])
     }
   }
